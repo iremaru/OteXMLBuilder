801efd34f3e36336fea80d088dcea079b95c9538
Iremaru <irenemarreroruiz@gmail.com>
2023-11-24 22:32:15 +0000

    updated ui styles

--- src/main/java/com/ote/ote/App.java
+++ src/main/java/com/ote/ote/App.java
@@ -8,11 +8,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class App extends Application {
+    String APP_NAME = "RExENI";
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("app-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("OTE");
+        stage.setTitle(APP_NAME);
         stage.setScene(scene);
         stage.show();
     }
